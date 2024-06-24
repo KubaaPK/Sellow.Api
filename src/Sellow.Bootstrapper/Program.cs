e8f3062de9853bb622f03aeba75035cadd38193f
Jakub Szkowrytko <k.szkowrytko@gmail.com>
2024-06-24 15:52:01 +0200

    feat: add Auth Module

--- src/Sellow.Bootstrapper/Program.cs
+++ src/Sellow.Bootstrapper/Program.cs
@@ -1,14 +1,19 @@
+using Sellow.Modules.Auth.Api;
 using Sellow.Shared.Infrastructure;
 using Sellow.Shared.Infrastructure.Logging;
 
 var builder = WebApplication.CreateBuilder(args).AddLogging();
 
 builder.Services
-    .AddSharedInfrastructure();
+    .AddSharedInfrastructure()
+    .AddAuthModule();
 
 var app = builder.Build();
 
+app.MapControllers();
+
 app
-    .UseSharedInfrastructure();
+    .UseSharedInfrastructure()
+    .UseAuthModule();
 
 app.Run();
